e611788ff6e76ad2d944edd0ed015c48da06b930
Michaylah Mam <mchylh@gmail.com>
2020-02-22 17:29:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,6 @@
 class Dog
-  attr_reader :name
-  def initialize(name)
-    @name = name
   end
 
-  fido = Dog.one(name="Fido")
-  snoopy = Dog.two(name="Snoopy")
-  lassie = Dog.three(name="Lassie")
+  fido = Dog.new
+  snoopy = Dog.new
+  lassie = Dog.new
